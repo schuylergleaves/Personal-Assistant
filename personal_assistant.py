8c0eb0eecc60d15e43ab171e4681b4d50e2de68f
schuylergleaves <schuylergleaves@ufl.edu>
2019-05-13 00:13:51 -0400

    restructured codebase to be modular - added TODO list

--- personal_assistant.py
+++ personal_assistant.py
@@ -8,22 +8,22 @@ import pyttsx3 as text_to_speech
 class PersonalAssistant:
     def __init__(self):
         self.active = True
-        self.action_list = {}
-        self.speech_list = {}
+        self.phrase_list = {}
         self.recognizer = speech_recognition.Recognizer()
         self.engine = text_to_speech.init()
 
         self.set_properties()
-        self.setup_action_list()
-        self.setup_speech_list()
+        self.setup_phrase_list()
 
         self.speak("Initializing personal assistant")
-        self.speak("Hello " + config.OWNER_NAME + " ,how may I help you today?")
+        self.speak("Hello " + config.OWNER_NAME + " how may I help you today?")
 
+    def run(self):
+        while self.is_active():
+            input = self.listen_for_audio()
+            if input is not None:
+                self.handle_audio(input)
 
-    # -------------------
-    # MAIN LOOP FUNCTIONS
-    # -------------------
     def listen_for_audio(self):
         with speech_recognition.Microphone() as source:
             self.recognizer.adjust_for_ambient_noise(source, 1)
@@ -32,11 +32,8 @@ class PersonalAssistant:
         return audio
 
     def handle_audio(self, audio):
-        if audio is None:
-            return
-        else:
-            user_message = self.convert_to_text(audio)
-            self.determine_action(user_message)
+        user_message = self.convert_to_text(audio)
+        self.determine_action(user_message)
 
     def convert_to_text(self, audio):
         try:
@@ -53,19 +50,15 @@ class PersonalAssistant:
         if user_message is None:
             return
 
-        for phrase in self.action_list:
+        for phrase in self.phrase_list:
             if self.message_contains_phrase(user_message, phrase):
                 self.perform_action(phrase, user_message)
-                self.speak(self.speech_list[phrase])
+                self.perform_dialogue_for_action(phrase, user_message)
 
     def speak(self, text):
         self.engine.say(text)
         self.engine.runAndWait()
 
-
-    # -----------------------
-    # INIT / HELPER FUNCTIONS
-    # -----------------------
     def is_active(self):
         return self.active
 
@@ -76,27 +69,37 @@ class PersonalAssistant:
     def message_contains_phrase(self, user_message, string):
         return re.search(string, user_message, re.IGNORECASE)
 
-    def setup_speech_list(self):
-        self.speech_list = {
-            "Google": "Opening a new tab",
-            "New Tab": "Opening a new tab",
-            "shut down": "Shutting down. Have a nice day!",
+    def setup_phrase_list(self):
+        """
+        The phrase list contains all phrases which the personal assistant will recognize.
+        Each phrase has an associated ACTION (something to perform) and a DIALOGUE (spoken to user by text to speech)
+        """
+        self.phrase_list = {
+            "Google": {
+                "ACTION": self.open_chrome_tab,
+                "DIALOGUE": "Opening a new tab"
+            },
+
+            "New Tab": {
+                "ACTION": self.open_chrome_tab,
+                "DIALOGUE": "Opening a new tab"
+            },
+
+            "Shut Down": {
+                "ACTION": self.shutdown,
+                "DIALOGUE": "Shutting down. Have a nice day!"
+            },
         }
 
 
-
-    # ------------------------------
-    # ACTIONS / HANDLING ACTION LIST
-    # ------------------------------
-    def setup_action_list(self):
-        self.action_list = {
-            "Google": self.open_chrome_tab,
-            "New Tab": self.open_chrome_tab,
-            "shut down": self.shutdown,
-        }
-
+    # -------
+    # ACTIONS
+    # -------
     def perform_action(self, phrase, user_message):
-        self.action_list[phrase](phrase, user_message)
+        self.phrase_list[phrase]['ACTION'](phrase, user_message)
+
+    def perform_dialogue_for_action(self, phrase, user_message):
+        self.speak(self.phrase_list[phrase]['DIALOGUE'])
 
     def open_chrome_tab(self, phrase, user_message):
         webbrowser.open_new_tab(config.GOOGLE_HOMEPAGE)
